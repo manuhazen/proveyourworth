0fbe7e369a99c531b37dd66a7267e245888c6677
Manu <manuhazen@outlook.com>
2018-08-12 16:47:08 -0400

    Trying again

--- payloadpost.py
+++ payloadpost.py
@@ -1,4 +1,13 @@
 import requests
 
-r = requests.post('http://www.proveyourworth.net/level3/reaper')
+url = 'http://www.proveyourworth.net/level3/reaper'
+payload = {
+    'email': 'manuhazen@outlook.com',
+    'name': 'Emmanuel Jimenez',
+    'image': 'https://github.com/manuhazen/proveyourworth/blob/master/watermarkedproveyourworth.jpg',
+    'resume': 'https://www.linkedin.com/nhome/?trk=',
+    'code': 'https://github.com/manuhazen/proveyourworth'
+}
+
+r = requests.post(url, params=payload)
 print(r.text)
